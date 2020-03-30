4f088a4216a53780149dd59ef6662f42a321e4c9
Harsh <harsh@zuru.tech>
2020-03-30 21:32:11 +0530

    feat: update folder structure

--- src/stories/index.js
+++ src/stories/index.js
@@ -10,14 +10,13 @@ import {Welcome } from '@storybook/react/demo';
 import withStyles from "@sambego/storybook-styles";
 import { setOptions } from '@storybook/addon-options';
 setOptions({ addonPanelInRight: true });
-
 import AvatarStory from '../stories/Avatar/index';
 import AvatarReadme from '../stories/Avatar/README.md';
 import AvatarDocs from '../stories/Avatar/DOCS.md';
 
-import Button from '../stories/Button/index';
-import ButtonReadme from '../stories/Button/README.md';
-import ButtonDocs from '../stories/Button/DOCS.md';
+import Button from '../components/Button/index';
+import ButtonReadme from '../components/Button/README.md';
+import ButtonDocs from '../components/Button/DOCS.md';
 
 const styles = {
   textAlign: 'center',
@@ -43,38 +42,37 @@ const variantOptions = {
  
 }
 // addDecorator(CenterDecorator);
-storiesOf('Welcome', module)
-  .add('to UI-Kit', () => <Welcome showApp={linkTo('Button')} />)
 
-storiesOf('Avatar', module)
-  .addDecorator(withKnobs)
-  .addDecorator(withDocs(AvatarDocs))
-  .addParameters({
-    styles:{
-      border:'1px solid black'
-    },
-    options: {
-      showPanel: true,
-      panelPosition: 'right',
-    },
-    readme: {
-      codeTheme: 'github',
-      sidebar: AvatarReadme,
-    },
-  })
-    // .addDecorator(withReadme(AvatarReadme))
-  .add('Avatar Usage',() => (
-    <AvatarStory
-      img={text('img', 'https://greatindiantours.com/wp-content/themes/travel-log/images/dummy-user.jpg')}
-      height={number('height', 60)}
-      width={number('width', 60)}
-      circle={boolean('circle', false)}
-      altText={text('altText', 'User Icon')}
-    />
-  ));
+// storiesOf('Avatar', module)
+//   .addDecorator(withKnobs)
+//   .addDecorator(withDocs(AvatarDocs))
+//   .addParameters({
+//     styles:{
+//       border:'1px solid black'
+//     },
+//     options: {
+//       showPanel: true,
+//       panelPosition: 'right',
+//     },
+//     readme: {
+//       codeTheme: 'github',
+//       sidebar: AvatarReadme,
+//     },
+//   })
+//     // .addDecorator(withReadme(AvatarReadme))
+//   .add('Avatar Usage',() => (
+//     <AvatarStory
+//       img={text('img', 'https://greatindiantours.com/wp-content/themes/travel-log/images/dummy-user.jpg')}
+//       height={number('height', 60)}
+//       width={number('width', 60)}
+//       circle={boolean('circle', false)}
+//       altText={text('altText', 'User Icon')}
+//     />
+//   ));
+
   storiesOf('Button',module)
   .addDecorator(withKnobs)
-  .addDecorator(withDocs(ButtonDocs))
+  // .addDecorator(withDocs(ButtonDocs))
   .addParameters({
     styles:{
       border:'1px solid black'
@@ -88,13 +86,102 @@ storiesOf('Avatar', module)
       sidebar: ButtonReadme,
     },
   })
-  .add('Button Usage',() => (
+  .add('short',() => (
             <Button
                     disabled={boolean('disabled',false)}
                     variant={select('variant',variantOptions)}
                     btnText={text('Text','Button')}
-                    type={select('type',options)}
+                    type={select('type',options, options.Short)}
+                    disableRipple={boolean('disableRipple',false)}
+                    fullWidth={boolean('fullWidth',false)}
+                    handleInputFocus={action('On Focus')}
+                    handleInputBlur={action('On Blur')}
+                    handleClick={action('Button Clicked')}
+                    >
+            </Button>
+  ))
+  .add('long',() => (
+            <Button
+                    disabled={boolean('disabled',false)}
+                    variant={select('variant',variantOptions)}
+                    btnText={text('Text','Button')}
+                    type={select('type',options, options.Long)}
                     disableRipple={boolean('disableRipple',true)}
+                    fullWidth={boolean('fullWidth',false)}
+                    handleInputFocus={action('On Focus')}
+                    handleInputBlur={action('On Blur')}
+                    handleClick={action('Button Clicked')}
+                    >
+            </Button>
+  ))
+
+  .add('alert',() => (
+            <Button
+                    disabled={boolean('disabled',false)}
+                    variant={select('variant',variantOptions)}
+                    btnText={text('Text','Button')}
+                    type={select('type',options, options.Alert)}
+                    disableRipple={boolean('disableRipple',true)}
+                    fullWidth={boolean('fullWidth',false)}
+                    handleInputFocus={action('On Focus')}
+                    handleInputBlur={action('On Blur')}
+                    handleClick={action('Button Clicked')}
+                    >
+            </Button>
+  ))
+
+  .add('outlined',() => (
+            <Button
+                    disabled={boolean('disabled',false)}
+                    variant={select('variant',variantOptions)}
+                    btnText={text('Text','Button')}
+                    type={select('type',options, options.Outlined)}
+                    disableRipple={boolean('disableRipple',true)}
+                    fullWidth={boolean('fullWidth',false)}
+                    handleInputFocus={action('On Focus')}
+                    handleInputBlur={action('On Blur')}
+                    handleClick={action('Button Clicked')}
+                    >
+            </Button>
+  ))
+
+  .add('link',() => (
+            <Button
+                    disabled={boolean('disabled',false)}
+                    variant={select('variant',variantOptions)}
+                    btnText={text('Text','Button')}
+                    type={select('type',options, options.Link)}
+                    disableRipple={boolean('disableRipple',true)}
+                    fullWidth={boolean('fullWidth',false)}
+                    handleInputFocus={action('On Focus')}
+                    handleInputBlur={action('On Blur')}
+                    handleClick={action('Button Clicked')}
+                    >
+            </Button>
+  ))
+
+  .add('line',() => (
+            <Button
+                    disabled={boolean('disabled',false)}
+                    variant={select('variant',variantOptions)}
+                    btnText={text('Text','Button')}
+                    type={select('type',options, options.Line)}
+                    disableRipple={boolean('disableRipple',false)}
+                    fullWidth={boolean('fullWidth',false)}
+                    handleInputFocus={action('On Focus')}
+                    handleInputBlur={action('On Blur')}
+                    handleClick={action('Button Clicked')}
+                    >
+            </Button>
+  ))
+
+  .add('icon',() => (
+            <Button
+                    disabled={boolean('disabled',false)}
+                    variant={select('variant',variantOptions)}
+                    btnText={text('Text','Button 😄')}
+                    type={select('type',options, options.Icon)}
+                    disableRipple={boolean('disableRipple',false)}
                     fullWidth={boolean('fullWidth',false)}
                     handleInputFocus={action('On Focus')}
                     handleInputBlur={action('On Blur')}
